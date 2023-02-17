7d851c50dfccfa1ea8c66dea7f83410186fc955f
root <amr.magdy.hassan95@gmail.com>
2023-02-17 12:34:26 -0800

    finally

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,12 +6,15 @@
 int main(void)
 {
 	int ch;
-	
+
 	for (ch = 0; ch <= 9; ch++)
 	{
 		putchar(48 + ch);
-		putchar(44);
-		putchar(32);
+		if (ch < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
 	putchar('\n');
 	return (0);

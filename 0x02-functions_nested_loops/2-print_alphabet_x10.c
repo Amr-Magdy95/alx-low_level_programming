a56fdf3717d4162ccad88356071a5b2260fae9e1
root <amr.magdy.hassan95@gmail.com>
2023-02-21 05:45:03 -0800

    alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing alphabets 10x times
+ * Return: returns nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}

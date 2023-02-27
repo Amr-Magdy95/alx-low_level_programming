5dabc65502f77db1750a164ec8b5387a5eb94a2f
root <amr.magdy.hassan95@gmail.com>
2023-02-27 10:28:33 -0800

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ * @str: char ptr
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, n = 0;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+		n++;
+	}
+	i = n - 1;
+	i = i / 2;
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+
+
+}

933fbd3ab6d86687d992ce35a822a3b7bf69b304
root <amr.magdy.hassan95@gmail.com>
2023-02-27 05:39:51 -0800

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - puts2
+ * @str: char ptr
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (true)
+	{
+		if (*(str + i) != '\0' || *(str + i - 1) != '\0')
+		{
+			break;
+		}
+		_putchar(*(str + i));
+		i += 2;
+	}
+}

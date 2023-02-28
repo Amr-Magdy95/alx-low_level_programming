47d0bddff2e0f8a545ded4028e5124e4d1868030
root <amr.magdy.hassan95@gmail.com>
2023-02-28 05:50:28 -0800

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string
+ * @dest: char ptr
+ * @src: char str
+ * Return: returns ptr the char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, j = 1;
+
+	while (j == 1)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+
+	}
+
+	return (*dest);
+}

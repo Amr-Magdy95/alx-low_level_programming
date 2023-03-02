f82c3357497360adc53db935e1b6546149321f25
root <amr.magdy.hassan95@gmail.com>
2023-03-02 03:08:18 -0800

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - descriptive
+ * @s: char ptr
+ * Return: char ptr
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' ||
+		    s[i] == '!' ||
+		    s[i] == ',' ||
+		    s[i] == ';' ||
+		    s[i] == '.' ||
+		    s[i] == '?' ||
+		    s[i] == '\"' ||
+		    s[i] == '(' ||
+		    s[i] == '(' ||
+		    s[i] == ')' ||
+		    s[i] == '{' ||
+		    s[i] == '}')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] -= 32;
+			}
+
+		}
+		return (s);
+}

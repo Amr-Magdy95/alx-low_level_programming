482ccaf0139f5d621f1fd462adebe6a3051345c5
root <amr.magdy.hassan95@gmail.com>
2023-03-06 04:06:48 -0800

    2finally

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 /**
  *_strchr - descriptive
  *@s: char ptr
@@ -18,5 +17,5 @@ char *_strchr(char *s, char c)
 		}
 		i++;
 	}
-	return (NULL);
+	return (0);
 }

33b60ba2d2d3d4eb0e7cf5be12cb496b84edb373
root <amr.magdy.hassan95@gmail.com>
2023-03-06 06:47:05 -0800

    7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_chessboard - desc
+ *@a: char ptr to ptr
+ *Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	for (; i < 8; i++)
+	{
+		for (; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}

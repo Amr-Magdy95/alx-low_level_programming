853ad3ebcc5b6b67a12e7e1c57a44aae6cfab1f9
root <amr.magdy.hassan95@gmail.com>
2023-03-06 05:54:56 -0800

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,26 +1,5 @@
 #include "main.h"
 /**
- *findChar - escriptive
- *@c: char
- *@s: char ptr
- *Return: unsigned int
- */
-unsigned int findChar(char c, char *s)
-{
-	unsigned int i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-		{
-			return (i + 1);
-		}
-		i++;
-	}
-	return (0);
-}
-
-/**
  *_strspn - descriptive
  *@s: chat ptr
  *@accept: char ptr
@@ -29,18 +8,26 @@ unsigned int findChar(char c, char *s)
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
-	unsigned int res = 0;
+	unsigned int i = 0;
 
-	while (accept[i] != '\0')
+	while (s[i] != '\0')
 	{
-		unsigned int n = findChar(accept[i], s);
+		bool found = false;
+		int j = 0;
 
-		if (res < n)
+		while (accept[j] != '\0')
 		{
-			res = n;
+			if (s[i] == accept[j])
+			{
+				found = true;
+			}
+			j++;
+		}
+
+		if (!found)
+		{
+			return (i);
 		}
 		i++;
 	}
-	return (res);
 }

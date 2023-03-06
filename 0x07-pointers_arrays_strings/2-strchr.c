b4ee1247aa1c59692129779cf3a43be7828e4e44
root <amr.magdy.hassan95@gmail.com>
2023-03-06 03:50:41 -0800

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strchr - descriptive
+ *@s: char ptr
+ *@c: char
+ *Return: char ptr
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (null);
+}

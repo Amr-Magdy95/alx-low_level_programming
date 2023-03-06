a12cdf4e501935390abfd84bb68f025cc36908c3
root <amr.magdy.hassan95@gmail.com>
2023-03-06 02:33:56 -0800

    0again

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
- * _memset - set memory
- * @s: char ptr
- * @c: char 
- * @n: unsigned int
- * Return: char ptr
+ *_memset - set memory
+ *@s: char ptr
+ *@b: char
+ *@n: unsigned int
+ *Return: char ptr
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;

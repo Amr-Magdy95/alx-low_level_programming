a7a991f57e6f99fc8cea700caf5debe4245f458b
root <amr.magdy.hassan95@gmail.com>
2023-03-10 04:58:39 -0800

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: ctr
+ * @argv: string array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}

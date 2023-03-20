1afcaf488661a6f0eb508f69f14538b683c094a3
root <amr.magdy.hassan95@gmail.com>
2023-03-20 07:11:53 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - desc
+ * @name: char *
+ * @age: float
+ * @owner: char ptr
+ *
+ * Description: long desc
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
+

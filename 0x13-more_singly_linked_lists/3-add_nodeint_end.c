87a65802cfd285064d48910a54b78f0229aff735
root <amr.magdy.hassan95@gmail.com>
2023-04-03 16:02:53 -0700

    3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,7 +18,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (newNode == NULL)
 		return (NULL);
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		*head = newNode;
 		return (newNode);

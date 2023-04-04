0bd51969aee0d6640e12d85d606bd3077826a12e
root <amr.magdy.hassan95@gmail.com>
2023-04-04 14:20:23 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newNode->n = n;
 
-	if (newNode == NULL)
+	if (newNode == NULL || head == NULL)
 		return (NULL);
 
 	while (temp != NULL)

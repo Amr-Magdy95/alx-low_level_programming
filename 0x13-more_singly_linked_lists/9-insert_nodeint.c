e780d5a7c289690e167d9eca79955aac09061358
root <amr.magdy.hassan95@gmail.com>
2023-04-04 14:05:43 -0700

    9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - desc
+ * @head: head of LL
+ * @idx: target idx
+ * @n: value of new node
+ * Return: address of new node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0, j = 1;
+	listint_t *temp = *head, *temp2,  *newNode = malloc(sizeof(listint_t));
+
+	while (j == 1)
+	{
+		if (i == idx - 1)
+		{
+			break;
+		}
+		i++;
+		temp = temp->next;
+
+	}
+	temp2 = temp->next;
+	temp->next = newNode;
+	newNode->n = n;
+	newNode->next = temp2;
+	return (newNode);
+}

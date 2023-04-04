ea7210e2acdf64539c052382cb483ec26d630179
root <amr.magdy.hassan95@gmail.com>
2023-04-04 14:13:43 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (temp != NULL)
 	{
 		k++;
-		temp = temp->next
+		temp = temp->next;
 	}
 	if (idx >= k)
 		return (NULL);

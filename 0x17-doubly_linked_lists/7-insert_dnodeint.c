644e36f64702accbefe92a3226d3cbd46b3bf471
root <amr.magdy.hassan95@gmail.com>
2023-05-19 12:46:28 -0700

    7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - fn
+ * @h: head
+ * @idx: index 
+ * @n: data
+ * Return: new node or null
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newNode, *curr = (*h), *prev;
+	unsigned int i = 0;
+
+	if ( (*h) == NULL)
+	{
+		return (NULL);
+	}
+	newNode = malloc(sizeof(dlistint_t));
+	newNode->n = n;
+	while (i < idx)
+	{
+		curr = curr->next;
+		if (curr == NULL)
+			return (NULL);
+		i++;
+	}
+	prev = curr->prev;
+	prev->next = newNode;
+	curr->prev = newNode;
+	newNode->next = curr;
+	return (newNode);
+
+
+}

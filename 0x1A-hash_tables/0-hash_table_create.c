9dd6ec8ad62adaeef37b062a02ce109024984d5a
root <amr.magdy.hassan95@gmail.com>
2023-06-11 03:40:43 -0700

    0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create a new hash table
+ * @size: number of buckets
+ * Return: the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_s));
+	if (!table->array)
+		return (NULL);
+	return (table);
+}

dd2e023367c42b4a46f4e83c45e7f6d87345eb4b
root <amr.magdy.hassan95@gmail.com>
2023-06-11 05:45:24 -0700

    3x2

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,37 @@
 #include "hash_tables.h"
 /**
+ * create_node - create a new node
+ * @key: key of node
+ * @value: value of the node
+ * Return: node for success - null for failure
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = malloc(strlen(key) + 1);
+	node->value = malloc(strlen(value) + 1);
+	if (node->key == NULL || node->value == NULL)
+		return (NULL);
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+	return (node);
+}
+/**
+ * free_node - free node
+ * @node: the node to be freed
+ * Return: void
+ */
+void free_node(hash_node_t *node)
+{
+	free(node->key);
+	free(node->value);
+	free(node);
+}
+/**
  * hash_table_set - setting a hash table node
  * @ht: hash table
  * @key: key of node
@@ -14,14 +46,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (strcmp(key, "") == 0)
 		return (0);
 
-	idx = key_index(key);
-	newNode = malloc(sizeof(hash_node_t));
+	idx = key_index((const unsigned char *)key, ht->size);
+	newNode = create_node(key, value);
 	curr = ht->array[idx];
 
 	if (newNode == NULL)
 		return (0);
-	newNode->key = key;
-	newNode->value = value;
 	if (curr == NULL)
 	{
 		ht->array[idx] = newNode;
@@ -31,9 +61,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		temp = curr;
 		while (temp)
 		{
-			if (temp->key == newNode->key)
+			if ((strcmp(temp->key, newNode->key)) == 0)
 			{
-				temp->value = newNode->value;
+				strcpy(temp->value, newNode->value);
+				free_node(newNode);
 				return (1);
 			}
 			newNode->next = curr;

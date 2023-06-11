4e09ef4d680f72c6153a4d94c9709941b79f96f2
root <amr.magdy.hassan95@gmail.com>
2023-06-11 06:05:15 -0700

    3x6

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -74,7 +74,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			}
 			temp = temp->next;
 		}
-		newNode->next = curr;
+		newNode->next = ht->array[idx];
 		ht->array[idx] = newNode;
 
 	}
